254f4d1f613ae4ce5d85e4930521a0cf7439fa53
Josh Rowley <joshua.f.rowley@gmail.com>
2016-08-17 22:21:04 -0400

    inital commit

---
+++ spec/single-sock-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SingleSockView from '../lib/single-sock-view';
+
+describe('SingleSockView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
