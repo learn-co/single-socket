75b06f80295706cfb36931675d59d6af0b1d14bd
Josh Rowley <joshua.f.rowley@gmail.com>
2016-08-23 22:51:32 -0400

    call onopen only if defined; add log helper

--- test/test.js
+++ test/test.js
@@ -1,15 +1,19 @@
 const path = require('path')
 const fork = require('child_process').fork
 
+global.log = function log() {
+  console.log.apply(arguments)
+}
+
 before(function(done) {
   var port = 8001
-  console.log('starting target websocket server on port ' + port)
+  log('starting target websocket server on port ' + port)
   this.wsServer = fork(path.join(__dirname, 'ws-server.js'), [port])
   setTimeout(done, 1500)
 })
 
 after(function() {
-  console.log('killing websocket server')
+  log('killing websocket server')
   this.wsServer.kill()
 })
 
