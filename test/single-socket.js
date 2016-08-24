24b2cc8a479e5e1c041a9111fbed28550386210c
Josh Rowley <joshua.f.rowley@gmail.com>
2016-08-23 23:10:20 -0400

    improve onopen test

--- test/single-socket.js
+++ test/single-socket.js
@@ -3,17 +3,14 @@ const expect = require('chai').expect
 const sinon = require('sinon')
 
 it('connects to the target ws server', function(done) {
-  var onopenSpy = sinon.spy()
+  this.timeout(5000)
+  var spy = sinon.spy()
 
   var socket = new SingleSocket('ws://localhost:8001', {
     onopen: function() {
-      console.log('on open executed')
-      onopenSpy()
+      spy()
+      expect(spy.calledOnce).to.be.true
+      done()
     }
   })
-
-  setTimeout(function() {
-    expect(onopenSpy.calledOnce).to.be.true
-    done()
-  }, 1500)
 })
