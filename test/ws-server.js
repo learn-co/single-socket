1e12511a2b5ae0c9f07fbd898e8e13e253ce92b0
Josh Rowley <joshua.f.rowley@gmail.com>
2016-09-28 18:02:51 -0400

    update websocket lib with ws

--- test/ws-server.js
+++ test/ws-server.js
@@ -1,20 +1,10 @@
 const http = require('http')
-const WebSocketServer = require('websocket').server
+const WebSocketServer = require('ws').Server
 
 var port = process.argv[2]
 
-var server = http.createServer(function(request, response) {
-  response.writeHead(404);
-  response.end();
-})
+var wsServer = new WebSocketServer({ port: port })
 
-server.listen(port)
-
-var wsServer = new WebSocketServer({
-  httpServer: server,
-  autoAcceptConnections: true
-})
-
-wsServer.on('connect', function(connection) {
-  connection.send(JSON.stringify({ping: 'pong'}))
+wsServer.on('connection', function(ws) {
+  ws.send(JSON.stringify({ping: 'pong'}))
 })
